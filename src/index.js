72059908183fbad043e012d48ec0722c4fc38b84
ELIUS <eliuswanyoro@gmail.com>
2022-08-22 15:29:40 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+    const  form = document.querySelector('#create-task-form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const list = document.createElement('li') 
+      const tasks = document.getElementById("tasks")
+      const input = document.getElementById("new-task-description")
+      list.textContent = input.value
+      tasks.appendChild(list)
+      return e.target.input.value
+    })
+
+})
+   
+
+
+
+
+
