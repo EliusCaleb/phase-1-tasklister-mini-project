b60b6f353ecb546fc5ad75da1b0d6ff6994f96f8
ELIUS <eliuswanyoro@gmail.com>
2022-08-24 15:03:02 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-    const  form = document.querySelector('#create-task-form')
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      const list = document.createElement('li') 
-      const tasks = document.getElementById("tasks")
-      const input = document.getElementById("new-task-description")
-      list.textContent = input.value
-      tasks.appendChild(list)
-      return e.target.input.value
-    })
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let li = document.createElement("li");
+    const userInput = document.getElementById("new-task-description");
+    li.innerText = userInput.value;
+    let ul = document.getElementById("tasks");
+    ul.append(li);
+    const button = document.createElement("button");
+    button.innerText = "X";
+    const div = document.getElementById("list");
+    li.appendChild(button);
+    button.addEventListener("click", () => {
+      li.remove();
+    });
+  });
+});
 
-})
-   
-
-
-
-
-
+     
